4b504c6e23d70e96e6349f813ad3fd6e723005d5
gdmec07150917 <1156749399@qq.com>
2016-10-29 14:34:33 +0800

    Initial commit

---
+++ app/src/main/java/com/example/huang/menu/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.huang.menu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private TextView tv1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv1=(TextView)findViewById(R.id.textView);
+        registerForContextMenu(tv1);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //MenuInflater mi=getMenuInflater();
+        //mi.inflate(R.menu.menu,menu);
+        menu.add(0,1,0,R.string.m1);
+        menu.add(0,1,0,R.string.m2);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Toast.makeText(this,item.getTitle().toString(),Toast.LENGTH_SHORT).show();
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+       Toast.makeText(this,item.getTitle(),Toast.LENGTH_LONG).show();
+        return super.onContextItemSelected(item);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.add(0,1,0,R.string.m1);
+        menu.add(0,1,0,R.string.m2);
+
+
+    }
+}
